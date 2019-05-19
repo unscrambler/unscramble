9106aac6d786eb660febaf4d06e652777c468a0d
unscrambler <unscramble@burakbayer.com>
2019-05-19 18:53:39 +0300

    sitemap updated.

--- sitemap.sh
+++ sitemap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 date=`date +%Y-%m-%d`
-echo "<?xml version="1.0" encoding="UTF-8"?>"
+echo '<?xml version="1.0" encoding="UTF-8"?>'
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
 
-for i in `ls docs/*.html|xargs basename`;
+for i in `ls docs/unscramble_*.html docs/index.html|xargs basename`;
 do echo "
  <url>
     <loc>$1/$i</loc>
