61fa9359ed91e45b5f429e5815de7b396cf462aa
Burak Bayer <bbayer82@gmail.com>
2019-06-08 11:53:03 +0300

    default argument added

--- sitemap.sh
+++ sitemap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -eq 0 ];then
+  sitename="https://unscrambler.github.io/unscramble"
+else
+  sitename=$1
+fi
 date=`date +%Y-%m-%d`
 echo '<?xml version="1.0" encoding="UTF-8"?>'
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
@@ -7,7 +12,7 @@ echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
 for i in `find docs/unscramble_* -print0 | xargs -0 -n1 basename`;
 do echo "
  <url>
-    <loc>$1/$i</loc>
+    <loc>$sitename/$i</loc>
     <lastmod>$date</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
